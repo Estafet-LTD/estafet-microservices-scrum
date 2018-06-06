cfa1830ad696f51f5d15355a84e9735a58e3735c
Dennis Williams <dennyjoewilliams@yahoo.com>
2018-06-06 13:16:54 +0100

    using split

--- ansible/deploy-prod-pipeline.groovy
+++ ansible/deploy-prod-pipeline.groovy
@@ -2,14 +2,16 @@ node {
 	sh "oc get dc --selector product=microservices-scrum -n prod > dc.output"
 	def dc = readFile('dc.output')
 	println dc
-	dc.splitEachLine('\n') { line, count ->
+	def count = 0
+	dc.split('\n').each {
     if (count > 0) {
-        def matcher = line =~ /(\w+\-\w+)(.*)/
+        def matcher = it =~ /(\w+\-\w+)(.*)/
         def microservice = matcher[0][1]
         println "deploying ${microservice} ..."
         openshiftDeploy namespace: "prod", depCfg: microservice
         openshiftVerifyDeployment namespace: "prod", depCfg: microservice, replicaCount:"1", verifyReplicaCount: "true", waitTime: "600000"
         println "deployed ${microservice}"
     }
+    count++
 	}
 }
