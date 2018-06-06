97d6ffc86300e6c593ec73995bc54279d642092e
Dennis Williams <dennyjoewilliams@yahoo.com>
2018-06-06 13:12:50 +0100

    using splitEachLine

--- ansible/deploy-prod-pipeline.groovy
+++ ansible/deploy-prod-pipeline.groovy
@@ -2,7 +2,7 @@ node {
 	sh "oc get dc --selector product=microservices-scrum -n prod > dc.output"
 	def dc = readFile('dc.output')
 	println dc
-	dc.split('\n') { line, count ->
+	dc.splitEachLine('\n') { line, count ->
     if (count > 0) {
         def matcher = line =~ /(\w+\-\w+)(.*)/
         def microservice = matcher[0][1]
