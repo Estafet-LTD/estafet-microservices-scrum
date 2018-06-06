f7f3c0111c033b667d31f62241ff0e5b25f5a3a6
Dennis Williams <dennis.williams@estafet.com>
2018-06-06 13:08:58 +0100

    normalise strings before processing

--- ansible/deploy-prod-pipeline.groovy
+++ ansible/deploy-prod-pipeline.groovy
@@ -2,7 +2,7 @@ node {
 	sh "oc get dc --selector product=microservices-scrum -n prod > dc.output"
 	def dc = readFile('dc.output')
 	println dc
-	dc.eachLine { line, count ->
+	dc.normalize().eachLine { line, count ->
     if (count > 0) {
         def matcher = line =~ /(\w+\-\w+)(.*)/
         def microservice = matcher[0][1]
@@ -13,4 +13,3 @@ node {
     }
 	}
 }
-
