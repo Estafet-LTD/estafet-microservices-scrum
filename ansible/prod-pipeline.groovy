cae9e8985793e99c4ab9331e0255c6d25f7a6b34
Dennis Williams <dennis.williams@estafet.com>
2018-06-07 18:43:08 +0100

    updated prod deploy pipeline

--- ansible/prod-pipeline.groovy
+++ ansible/prod-pipeline.groovy
@@ -1,15 +1,23 @@
-node {
-	sh "oc get dc --selector product=microservices-scrum -n prod > dc.output"
-	def dc = readFile('dc.output')
-	println dc
+@NonCPS
+def getDeploymentConfigs(dc) {
 	def count = 0
-	dc.split('\n').each {
+	def configs = []
+	dc.split('\n').each { line ->
     if (count > 0) {
-        def matcher = it =~ /(\w+\-\w+)(.*)/
-        def microservice = matcher[0][1]
-        openshiftDeploy namespace: "prod", depCfg: microservice
-        openshiftVerifyDeployment namespace: "prod", depCfg: microservice, replicaCount:"1", verifyReplicaCount: "true", waitTime: "600000"
+        def matcher = line =~ /(\w+\-\w+)(.*)/
+        configs << matcher[0][1]
     }
     count++
 	}
+	return configs
+}
+
+node {
+	sh "oc get dc --selector product=microservices-scrum -n prod > dc.output"
+	def dc = readFile('dc.output')
+	def configs = getDeploymentConfigs dc
+	configs.each { microservice ->
+        openshiftDeploy namespace: "prod", depCfg: microservice
+        openshiftVerifyDeployment namespace: "prod", depCfg: microservice, replicaCount:"1", verifyReplicaCount: "true", waitTime: "600000"
+    }
 }
