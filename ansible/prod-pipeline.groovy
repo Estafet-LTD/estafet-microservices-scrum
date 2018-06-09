b8d25829d8ae832e60b9742a83fe2aec68d48fe4
Dennis Williams <dennyjoewilliams@yahoo.com>
2018-06-09 15:15:06 +0100

    modified deployment pipeline so that only the latest image is deployed

--- ansible/prod-pipeline.groovy
+++ ansible/prod-pipeline.groovy
@@ -28,6 +28,55 @@ def getImageStreams(is) {
 	return imageStreams
 }
 
+@NonCPS
+def getImage(json) {
+	def imageId = new groovy.json.JsonSlurper().parseText(json).status.containerStatuses[0].imageID
+	def matcher = imageId =~ /(.*\@sha256\:)(\w+)/
+	return matcher[0][2]
+}
+
+@NonCPS
+def getLatest(json) {
+	def tags = new groovy.json.JsonSlurper().parseText(json).status.tags
+	tags.each {
+		if (it.tag.equals("latest")) {
+			return it.items[0].image
+		}
+	}
+	return null
+}
+
+def isLatestImageDeployed(microservice) {
+	def pod = getPod microservice
+	def podImage = getPodImage pod
+	def latestImage = getLatestImage microservice
+	return podImage.equals(latestImage)
+}
+
+def getPod(microservice) {
+	sh "oc get pods --selector app=${microservice} > pod.output"
+	def pod = readFile('pod.output')
+	def lines = pod.split('\n')
+	if (lines.size() > 1) {
+		def matcher = lines[0] =~ /(\w+\-\w+)(.*)/
+		return matcher[0][1]
+	} else {
+		return null
+	}
+}
+
+def getPodImage(pod) {
+	sh "oc get pod ${pod} -o json > image.output"
+	def image = readFile('image.output')
+	return getImage image
+}
+
+def getLatestImage(microservice) {
+	sh "oc get is project-api -o json > latest.json"
+	def latest = readFile('latest.json')
+	return getLatest latest
+}
+
 node {
 	
 	stage ('deploy each microservice') {
@@ -38,11 +87,14 @@ node {
 		def dc = readFile('dc.output')
 		def configs = getDeploymentConfigs(dc, imageStreams)
 		configs.each { microservice ->
-					println microservice
-	        openshiftDeploy namespace: "prod", depCfg: microservice
-	        openshiftVerifyDeployment namespace: "prod", depCfg: microservice, replicaCount:"1", verifyReplicaCount: "true", waitTime: "600000"
+					if (isLatestImageDeployed(microservice)) {
+						println "deploying ${microservice}..."
+		        openshiftDeploy namespace: "prod", depCfg: microservice
+		        openshiftVerifyDeployment namespace: "prod", depCfg: microservice, replicaCount:"1", verifyReplicaCount: "true", waitTime: "600000"	
+		        println "${microservice} deployed"
+					} else {
+						println "${microservice} latest image is already deployed"
+					}
 	  }	
 	}
-	
-	
 }
