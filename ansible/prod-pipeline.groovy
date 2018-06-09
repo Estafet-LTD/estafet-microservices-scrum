1cfa3ecb90e7a1b44fc2fbbf3626646bbdee0976
Dennis Williams <dennyjoewilliams@yahoo.com>
2018-06-09 16:29:10 +0100

    lklklk

--- ansible/prod-pipeline.groovy
+++ ansible/prod-pipeline.groovy
@@ -66,10 +66,7 @@ def getPod(microservice) {
 }
 
 def getPodImage(pod) {
-	sh "oc version > version.output"
-	def version = readFile('version.output')
-	println version
-	sh "oc get pod ${pod} -o json -n prod"
+	sh "oc get -o json pod ${pod} -n prod"
 	sh "oc get pod ${pod} -o json -n prod > image.json"
 	def image = readFile('image.json')
 	return getImage(image)
