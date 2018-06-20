1242d8c3e625791759daeafc00b18ff8ba53d4ef
Dennis Williams <dennyjoewilliams@yahoo.com>
2018-06-20 16:45:52 +0100

    incorporate openshift environments configuration for aws cloud

--- create-environments.sh
+++ create-environments.sh
@@ -7,6 +7,6 @@ apt-get install python-jmespath
 git clone https://github.com/Estafet-LTD/estafet-microservices-scrum.git
 
 # Run the playbook.
-ANSIBLE_HOST_KEY_CHECKING=False /usr/local/bin/ansible-playbook -i ./environments-inventory.cfg ./estafet-microservices-scrum/ansible/create-local-environment-playbook.yml
+ANSIBLE_HOST_KEY_CHECKING=False /usr/local/bin/ansible-playbook -i ./environments-inventory.cfg ./estafet-microservices-scrum/ansible/create-devops-environments-playbook.yml
 
 
