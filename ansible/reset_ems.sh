e6ee71a59a3f5ccaa1cd0ded51991a68933725e3
Steve Brown <steve.brown@estafet.com>
2019-09-15 17:38:13 +0100

    reset_ems.sh now uses Ansible.

--- ansible/reset_ems.sh
+++ ansible/reset_ems.sh
@@ -2,57 +2,6 @@
 
 DEBUG=false
 
-function delete_project() {
-
-    local name"=$1"
-
-    echo "Deleting project ${name} (will wait up to 60s) ..."
-    local result=
-
-    result="$(oc delete project --grace-period 60 "${name}" 2>&1)"
-    local -i status=$?
-
-    ${DEBUG} && echo "${result}"
-    if [ ${status} -eq 0 ]; then
-        echo "Marked ${name} project for deletion."
-    else
-        echo "ERROR: Failed to mark ${name} project for deletion."
-    fi
-    return ${status}
-}
-
-function project_exists() {
-    local name"=$1"
-
-    local result=
-
-    result="$(oc get project "${name}" 2>&1)"
-    local -i status=$?
-
-    ${DEBUG} && echo "${result}"
-
-    return ${status}
-}
-
-function wait_for_delete() {
-
-    local name="$1"
-
-    local -i interval=10
-    local -i limit=300
-    while [ ${limit} -gt 0 ]
-    do
-        sleep ${interval}
-        project_exists "${name}" || {
-        	echo "INFO: The ${name} project has been deleted."
-        	return 0
-    	}
-    	((limit-=interval))
-    done
-    echo "ERROR: project ${name} exists after 300s."
-    return 1
-}
-
 function login() {
     oc login -u admin -p 123 --insecure-skip-tls-verify=true https://ip-10-0-1-105.eu-west-2.compute.internal:8443 || {
 	    echo "ERROR: Failed to login as admin."
@@ -67,34 +16,19 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" || {
     exit 1
 }
 
-declare projects="prod test dev cicd"
+pushd "${DIR} >/dev/null 2>&1 || {
+	echo "ERROR: failed to pushd $DIR}."
+	exit 1
+}
 
-login || exit 1
+trap 'popd >/dev/null 2>&1' 0
+trap "exit 2" 1 2 3 13 15
 
-echo "INFO: Resetting Estafet Microservice Scrum applications ..."
-((errors=0))
-for project in ${projects}; do
-	
-    if project_exists "${project}"; then
-        if delete_project "${project}"; then
-            wait_for_delete "${project}" || {
-            	echo "WARNING: Failed to delete the ${project} project."
-            	(( ++errors))
-            }
-        fi
-    else
-        echo "INFO: ${project} does not exist."
-    fi
-done
-
-oc logout
-
-if [[ ${errors} -eq 0 ]]; then
-    echo "INFO: Reset the Estafet Microservice Scrum applications OK."
-else
-    echo "INFO: Failed to reset all the Estafet Microservice Scrum applications."
-   	exit 1
-fi
+echo "INFO: Deleting all environements an their contents ..."
+ansible-playbook -vv delete-devops-environments-playbook.yml || {
+	echo "ERROR: Failed to delete all environments."
+	exit 1
+}
 
 echo "INFO: Dropping Estafet Microservice Scrum application databases ..."
 ansible-playbook -vv drop-postgres-databases-playbook.yml || {
