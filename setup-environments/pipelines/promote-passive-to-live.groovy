b73d38a90de1c1b3976c492f7628eace0968a2cb
Dennis Williams <dennyjoewilliams@yahoo.com>
2019-10-08 00:19:38 +0100

    fixed

--- setup-environments/pipelines/promote-passive-to-live.groovy
+++ setup-environments/pipelines/promote-passive-to-live.groovy
@@ -1,6 +1,6 @@
 @NonCPS
 def getTargetEnvironment(json) {
-	def matcher = new groovy.json.JsonSlurper().parseText(json).items[0].spec.to.name =~ /(green|blue)(\-basic\-ui)/
+	def matcher = new groovy.json.JsonSlurper().parseText(json).spec.to.name =~ /(green|blue)(basic\-ui)/
 	String namespace = matcher[0][1]
 	return namespace.equals("green") ? "blue" : "green" 
 }
@@ -10,16 +10,16 @@ node {
 	def env
 
 	stage("determine the environment to deploy to") {
-		sh "oc get route -o json -n prod --selector app=basic-ui > route.json"
+		sh "oc get route basic -o json -n prod > route.json"
 		def route = readFile('route.json')
 		env = getTargetEnvironment(route)
-		println "the target namespace to make active is prod-${env}"
+		println "the target deployment is $env"
 	}	
 	
-	stage("make the target namespace active") {
+	stage("make the target deployment active") {
 		sh "oc patch route/basic-ui -p '{\"spec\":{\"to\":{\"name\":\"${env}basic-ui\"}}}' -n live > route.out"
 		def route = readFile('route.out')
-		if (route.indexOf("basic-ui patched")< 0) {
+		if (route.indexOf("basic-ui patched") < 0) {
 			throw new RuntimeException("error when patching route $route")
 		}
 	}
