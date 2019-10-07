0533653918dba4ba20e7ab5ac5df7be7bfcdfa64
Dennis Williams <dennyjoewilliams@yahoo.com>
2019-10-07 14:09:36 +0100

    pipeline to make the passive environment live

---
+++ setup-environments/pipelines/promote-passive-to-live.groovy
@@ -0,0 +1,29 @@
+@NonCPS
+def getTargetEnvironment(json) {
+	def matcher = new groovy.json.JsonSlurper().parseText(json).items[0].spec.to.name =~ /(green|blue)(\-basic\-ui)/
+	String namespace = matcher[0][1]
+	return namespace.equals("green") ? "blue" : "green" 
+}
+
+node {
+	
+	def env
+
+	properties([
+	  parameters([
+	     string(name: 'GITHUB'),
+	  ])
+	])
+	
+	stage("determine the environment to deploy to") {
+		sh "oc get route -o json -n live > route.json"
+		def route = readFile('route.json')
+		env = getTargetEnvironment(route)
+		println "the target namespace to make active is prod-${env}"
+	}	
+	
+	stage("make the target namespace active") {
+		sh "oc patch route/ bluegreen-p '{\" spec\":{\" to\":{\" name\":\" ${env}-basic-ui\"}}}' -n live"
+	}
+	
+}	
