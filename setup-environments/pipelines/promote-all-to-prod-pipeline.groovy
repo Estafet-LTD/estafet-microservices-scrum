499e4de3ae73196239a5b18153e07effed0927aa
Dennis Williams <dennyjoewilliams@yahoo.com>
2019-10-09 11:21:30 +0100

    implementation for testing gateway from test to prod

--- setup-environments/pipelines/promote-all-to-prod-pipeline.groovy
+++ setup-environments/pipelines/promote-all-to-prod-pipeline.groovy
@@ -8,14 +8,35 @@ def getMicroServices(json) {
 	return microservices
 }
 
+@NonCPS
+def getTestStatus(json) {
+	def test-passed = new groovy.json.JsonSlurper().parseText(json).metadata.labels.test-passed
+	return test-passed
+}
+
 node {
-	stage ('deploy each microservice') {
-		sh "oc get is -n test --selector product=microservices-scrum -o json > images.output"
-		def images = readFile('images.output')
-		def microservices = getMicroServices(images)
-		microservices.each { microservice ->
-			openshiftBuild namespace: "cicd", buildConfig: "promote-to-prod-${microservice}", waitTime: "300000"
-			openshiftVerifyBuild namespace: "cicd", buildConfig: "promote-to-prod-${microservice}", waitTime: "300000" 
-	  }	
+	
+	def testStatus
+	
+	stage ("determine the status of the test environment") {
+		sh "oc get project test -o json > project.json"
+		def project = readFile('project.json')
+		testStatus = getTestStatus(project)
+		println "the target deployment is $testStatus"
+	}
+	
+	stage ('deploy each microservice to prod') {
+		if (testStatus.equals("true")) {
+			sh "oc get is -n test --selector product=microservices-scrum -o json > images.output"
+			def images = readFile('images.output')
+			def microservices = getMicroServices(images)
+			microservices.each { microservice ->
+				openshiftBuild namespace: "cicd", buildConfig: "promote-to-prod-${microservice}", waitTime: "300000"
+				openshiftVerifyBuild namespace: "cicd", buildConfig: "promote-to-prod-${microservice}", waitTime: "300000" 
+		  }		
+		}  else {
+			throw new RuntimeException("Cannot deploy microservices to production as the test environment has not been passed testing")
+		}
+		
 	}
 }
